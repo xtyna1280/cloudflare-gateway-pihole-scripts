77e9d41f5fc3ce333a1faab6e84bacdf8af5dcf6
xtyna1280 <141538300+xtyna1280@users.noreply.github.com>
2023-09-09 14:34:55 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -5,10 +5,8 @@ touch input.csv
 
 # declare an array of urls
 urls=(
-  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
-  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
-  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
 )
 
 # loop through the urls and download each file with curl
